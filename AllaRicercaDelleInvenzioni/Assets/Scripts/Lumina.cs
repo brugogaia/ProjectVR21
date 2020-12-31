d2ddf055cd93bb8412cb59f2a50b83fc35a2661f
mattnot3 <70361629+mattnot3@users.noreply.github.com>
2020-12-31 08:56:22 +0100

    Add files via upload

---
+++ AllaRicercaDelleInvenzioni/Assets/Scripts/Lumina.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lumina : MonoBehaviour
+{
+    public GameObject player;
+    private Color original;
+    private float Red;
+    private float Green;
+    private float Blue;
+    private float MaxDist = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        original = gameObject.GetComponent<Renderer>().material.color;
+        Red = original.r;
+        Green = original.g;
+        Blue = original.b;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 PlayPos=player.transform.position;
+        Vector3 Pos = transform.position;
+        float Distance = Vector3.Distance(PlayPos, Pos);
+        if (Distance < MaxDist)
+        {
+            Color Shiny = new Color(Red+(255f-Red)*(MaxDist-Distance)/MaxDist,Green+(255f-Green)*(MaxDist-Distance)/MaxDist,Blue+(255f-Blue)*(MaxDist-Distance)/MaxDist); 
+            Color newCol = Color.Lerp(original, Shiny, 0.01f);
+            gameObject.GetComponent<Renderer>().material.color=newCol;
+        }
+        else
+            gameObject.GetComponent<Renderer>().material.color=original;
+    }
+}
