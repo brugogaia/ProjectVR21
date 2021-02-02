1bc47947a31f75d17871b03690d4082c4b40ff93
mattnot <matteo.no98@gmail.com>
2021-02-02 15:46:40 +0100

    Grabbable

--- AllaRicercaDelleInvenzioni/Assets/Scripts/RayCast.cs
+++ AllaRicercaDelleInvenzioni/Assets/Scripts/RayCast.cs
@@ -12,8 +12,9 @@ public class RayCast : MonoBehaviour
     private CharacterController _fpsController;
     public Image _focus;
     private bool Grabbing = false;
-    private GameObject GrabbedObj;
+    private Grabbable GrabbedObj;
     private Transform OriginalParent;
+    private Grabbable _pointingGrabbable;
     private Color OriginalColor;
 
     void Start()
@@ -29,31 +30,36 @@ public class RayCast : MonoBehaviour
 
         RaycastHit hit;
 
+
+
         if (Physics.Raycast(ray, out hit, _raycastDistance))
         {
-            if (Input.GetMouseButtonDown(1) && Grabbing == false)
-                Grab(hit.transform.gameObject);
-            else if (Input.GetMouseButtonDown(0) && Grabbing == true)
-                Drop();
             _focus.color = new Color(1, 0, 0, 0.5f);
-        }
-        else
+            _pointingGrabbable = hit.transform.GetComponent<Grabbable>();
+            if (Input.GetMouseButtonDown(1) && Grabbing == false && _pointingGrabbable) {
+                
+                _pointingGrabbable.Grab(gameObject);
+                Grab(_pointingGrabbable);
+            }
+             
+        } else
             _focus.color = OriginalColor;
         Debug.DrawRay(rayOrigin, _fpsCameraT.forward * _raycastDistance, Color.blue);
 
+        if (Input.GetMouseButtonDown(0) && Grabbing == true)
+            Drop();
     }
 
-    private void Grab(GameObject grabable) {
-        OriginalParent = grabable.transform.parent;
-        grabable.transform.SetParent(_fpsCameraT);
-        grabable.GetComponent<Rigidbody>().isKinematic = true;
+    private void Grab(Grabbable grabbable) {
+        GrabbedObj = grabbable;
+        grabbable.transform.SetParent(_fpsCameraT);
         Grabbing = true;
-        GrabbedObj = grabable;
+
     }
 
     private void Drop() {
-        GrabbedObj.transform.parent = OriginalParent;
-        GrabbedObj.GetComponent<Rigidbody>().isKinematic = false;
+        GrabbedObj.Drop();
+        GrabbedObj.transform.parent = GrabbedObj.OriginalParent;
         Grabbing = false;
     }
 }
---
+++ AllaRicercaDelleInvenzioni/Assets/Scripts/SimpleGrabbable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleGrabbable : Grabbable
+{
+    private Rigidbody _rigidbody;
+    private Collider _collider;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        _collider = GetComponent<Collider>();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public override void Grab(GameObject grabber)
+    {
+        _collider.enabled = false;
+        _rigidbody.isKinematic = true;
+    }
+
+    public override void Drop()
+    {
+        _collider.enabled = true;
+        _rigidbody.isKinematic = false;
+    }
+}
