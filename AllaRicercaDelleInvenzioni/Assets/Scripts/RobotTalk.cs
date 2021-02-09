eb8ee9969d3d7764bc921e214074038adc9d6c26
brugogaia <brugogaia@users.noreply.github.com>
2021-02-09 09:46:53 +0100

    commit di cose  - prove

---
+++ AllaRicercaDelleInvenzioni/Assets/Scripts/RobotTalk.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RobotTalk : MonoBehaviour {
+
+    public Text textDisplay;
+    public Image e;
+    public string[] sentences;
+    public string[] indovinelli;
+    private int index;
+    
+
+    void Start()
+    {
+        e.enabled = false;
+        StartCoroutine(Type());
+    }
+
+    private void Update()
+    {
+        if (textDisplay.text == sentences[index])
+        {
+            e.enabled = true;
+            //if ()
+
+        }
+    }
+
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+} 
