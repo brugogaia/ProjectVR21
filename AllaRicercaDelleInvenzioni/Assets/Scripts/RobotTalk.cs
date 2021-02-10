9d9634328ca1c53fdfbdc3cb2491120b53f61097
brugogaia <brugogaia@users.noreply.github.com>
2021-02-10 16:10:22 +0100

    robottino talk e animator

--- AllaRicercaDelleInvenzioni/Assets/Scripts/RobotTalk.cs
+++ AllaRicercaDelleInvenzioni/Assets/Scripts/RobotTalk.cs
@@ -7,33 +7,137 @@ public class RobotTalk : MonoBehaviour {
 
     public Text textDisplay;
     public Image e;
+    public GameObject talk_box;
+    private AudioSource source;
+    private Animator anim;
     public string[] sentences;
-    public string[] indovinelli;
-    private int index;
-    
+    public string[] riddles;
+    public bool entrato;
+    public bool start;
+    private int index_s;
+    private int index_r;
+
 
     void Start()
     {
+        source = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
         e.enabled = false;
-        StartCoroutine(Type());
+        entrato = false;
+        start = true;
+        talk_box.SetActive(false);
+        textDisplay.enabled = false;
+
+        anim.SetBool("sleep", true);
+
     }
 
-    private void Update()
-    {
-        if (textDisplay.text == sentences[index])
-        {
-            e.enabled = true;
-            //if ()
+    private void Update() {
 
+        if (entrato && (index_s <= sentences.Length - 1))
+        {
+            if (start)
+            {
+                anim.SetBool("wakeup", true);
+                anim.SetBool("head", true);
+                anim.SetBool("sleep", false);
+                StartCoroutine(Type(sentences, index_s));
+                start = false;
+            }
+
+            if (textDisplay.text == sentences[index_s])
+            {
+                source.Stop();
+                anim.SetBool("head", false);
+                if (index_s < sentences.Length - 1)
+                {
+                    e.enabled = true;
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        NextSentence();
+                    }
+                }else if (index_s == sentences.Length - 1)
+                {
+                    index_s++;
+                }
+                
+            }
+        }
+        else if(entrato && (index_r <= riddles.Length - 1))
+        {
+            if (start)
+            {
+                StartCoroutine(Type(riddles, index_r));
+                start = false;
+            }
+
+            if (textDisplay.text == riddles[index_r])
+            {
+                source.Stop();
+                anim.SetBool("head", false);
+            }
         }
     }
 
-    IEnumerator Type()
+    IEnumerator Type(string[] phrase, int s)
     {
-        foreach(char letter in sentences[index].ToCharArray())
+        source.Play();
+        anim.SetBool("head", true);
+        foreach (char letter in phrase[s].ToCharArray())
         {
-            textDisplay.text += letter;
-            yield return new WaitForSeconds(0.05f);
+            if (entrato)
+            {
+                textDisplay.text += letter;
+                yield return new WaitForSeconds(0.05f);
+            }
+            
         }
     }
+
+    private void OnTriggerEnter(Collider player)
+    {
+
+        if (player.gameObject.tag == "Player")
+        {
+            if (!entrato)
+            {
+                start = true;
+            }
+
+            entrato = true;
+            talk_box.SetActive(true);
+            textDisplay.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            entrato = false;
+            talk_box.SetActive(false);
+            textDisplay.text = "";
+            textDisplay.enabled = false;
+            source.Stop();
+            anim.SetBool("head", false);
+        }
+    }
+
+    private void NextSentence()
+    {
+        index_s++;
+        e.enabled = false;
+        if (index_s <= sentences.Length - 1)
+        {
+            textDisplay.text = "";
+            StartCoroutine(Type(sentences, index_s));
+        }
+        else
+        {
+            textDisplay.text = "";
+            talk_box.SetActive(false);
+        }
+    }
+
+
 } 
