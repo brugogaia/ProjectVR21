e94fc6d116f067229fa33c57eec4bd7bde25a9e4
brugogaia <brugogaia@users.noreply.github.com>
2021-02-21 22:56:00 +0100

    luci e trigget bussola

---
+++ AllaRicercaDelleInvenzioni/Assets/Scripts/ChangeDirectionLab.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeDirectionLab : MonoBehaviour
+{
+    private bool entrato = false;
+    public GameObject text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        entrato = false;
+        text.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (entrato)
+        {
+            text.SetActive(true);
+        }
+        else
+        {
+            text.SetActive(false);
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider player)
+    {
+
+        if (player.gameObject.tag == "Player")
+        {
+            entrato = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            entrato = false;
+        }
+    }
+}
